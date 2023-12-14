34c1c4fbf40a5ff682b125cc011e3b4086e8386c
sahar.refua <sahar.refua@pipl.com>
2023-12-14 09:49:19 +0200

    complete Java course

--- JavaCourse/src/FinalExerises/FinalFunctions.java
+++ JavaCourse/src/FinalExerises/FinalFunctions.java
@@ -1,6 +1,7 @@
 package FinalExerises;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class FinalFunctions {
 
@@ -74,12 +75,135 @@ public class FinalFunctions {
 
     void middleNumber(double[] numbers) {
         if (numbers.length % 2 == 0) {
-            double middleNumber = numbers.length / 2;
+            double middleNumber = (numbers.length - 1) / 2;
+        } else {
+            double middleNumber = (numbers.length / 2) + 1;
         }
-
-
     }
 
 
+    void randomSongsWords(String songWords) {
+        // Create a Random object
+        Random random = new Random();
+        // Generate a random number between 0 (inclusive) and the length of the array (exclusive)
+        String[] list = songWords.split(" ");
+
+        for (int i = 0; i < list.length; i++) {
+            int randomNumber = random.nextInt(list.length);
+            System.out.println(list[randomNumber]);
+        }
+    }
+
+    void returnTheLongestString(String[] words) {
+        String longestWord = "";
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() > longestWord.length()) {
+                longestWord = words[i];
+            }
+        }
+        System.out.println("The longest word is: " + longestWord);
+    }
+
+    void findTheWord(String[] words, String word) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].contains(word)) {
+                System.out.println("The word " + words[i] + " is in the array");
+            }
+        }
+    }
+
+    void findTheBestStudent(String[][] grades) {
+        int bestGrade = 0;
+        String bestStudent = "";
+        for (int i = 0; i < grades.length; i++) {
+            if (Integer.parseInt(grades[i][1]) > bestGrade) {
+                bestGrade = Integer.parseInt(grades[i][1]);
+                bestStudent = grades[i][0];
+            }
+        }
+        System.out.println("The best student is: " + bestStudent + " with grade: " + bestGrade);
+    }
+
+    void findSecondBestNumber(double[] numbers) {
+        double secondBestNumber = Integer.MIN_VALUE;
+        double bestNumber = Integer.MIN_VALUE;
+        for (double number : numbers) {
+            if (number > bestNumber) {
+                secondBestNumber = bestNumber;
+                bestNumber = number;
+            } else if (number > secondBestNumber && number < bestNumber) {
+                secondBestNumber = number;
+            }
+        }
+        System.out.println("The second best number is: " + secondBestNumber);
+    }
+
+
+    void theArrayAreEqual(int[] numbers1, int[] numbers2) {
+        Boolean isEqual = true;
+        if (numbers1.length == numbers2.length) {
+            for (int number : numbers1) {
+                for (int number2 : numbers2) {
+                    if (number != number2) {
+                        isEqual = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if (isEqual)
+            System.out.println("The arrays are equal");
+        else
+            System.out.println("The arrays are not equal");
+    }
+
+    void returnPrimeNumbers(int[] numbers) {
+        for (int number : numbers) {
+            boolean isPrime = true;
+            for (int k = 2; k <= number / 2; k++) {
+                if (number % k == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                System.out.println("The number: " + number + " is prime");
+        }
+    }
+
+    static String convertToEnglish(int number) {
+        if (number == 0) {
+            return "Zero";
+        }
+
+        // Arrays to store words for numbers less than 20 and multiples of 10
+        String[] belowTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+
+        String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+        // Use recursion for numbers greater than 20
+        if (number < 20) {
+            return belowTwenty[number];
+        } else if (number < 100) {
+            return tens[number / 10] + " " + convertToEnglish(number % 10);
+        } else if (number < 1000) {
+            return belowTwenty[number / 100] + " Hundred " + convertToEnglish(number % 100);
+        } else if (number < 1000000) {
+            return convertToEnglish(number / 1000) + " Thousand " + convertToEnglish(number % 1000);
+        } else {
+            return "Number is too large for this example.";
+        }
+    }
+
+
+
+
+
+
 }
+
+
+
+
 
--- JavaCourse/src/FinalExerises/FinalMain.java
+++ JavaCourse/src/FinalExerises/FinalMain.java
@@ -21,6 +21,58 @@ public class FinalMain {
         ff.checkArray(evenNumbers);
         System.out.println("========= Part 7 ===========");
         ff.middleNumber(numbers);
+        System.out.println("========= Part 8 ===========");
+        String friendsThemeSongLyrics =
+                "So no one told you life was gonna be this way\n" +
+                        "Your job's a joke, you're broke, your love life's D.O.A.\n" +
+                        "It seems you're always stuck in second gear\n" +
+                        "When it hasn't been your day, your week, your month, or even your year, but\n\n" +
+                        "I'll be there for you\n(When the rain starts to pour)\n" +
+                        "I'll be there for you\n(Like I've been there before)\n" +
+                        "I'll be there for you\n('Cause you're there for me too)\n\n" +
+                        "You're still in bed at ten and work began at eight\n" +
+                        "You burned your breakfast so far, things are going great\n" +
+                        "Your mother warned you there'd be days like these\n" +
+                        "But she didn't tell you when the world has brought you down to your knees that\n\n" +
+                        "I'll be there for you\n(When the rain starts to pour)\n" +
+                        "I'll be there for you\n(Like I've been there before)\n" +
+                        "I'll be there for you\n('Cause you're there for me too)\n\n" +
+                        "No one could ever know me, no one could ever see me\n" +
+                        "Seems you're the only one who knows what it's like to be me\n" +
+                        "Someone to face the day with, make it through all the rest with\n" +
+                        "Someone I'll always laugh with, even at my worst, I'm best with you, yeah!\n\n" +
+                        "It's like you're always stuck in second gear\n" +
+                        "When it hasn't been your day, your week, your month, or even your year, but\n\n" +
+                        "I'll be there for you\n(When the rain starts to pour)\n" +
+                        "I'll be there for you\n(Like I've been there before)\n" +
+                        "I'll be there for you\n('Cause you're there for me too)\n\n" +
+                        "I'll be there for you\n(When the rain starts to pour)\n" +
+                        "I'll be there for you\n(Like I've been there before)\n" +
+                        "I'll be there for you\n('Cause you're there for me too)";
+        ff.randomSongsWords(friendsThemeSongLyrics);
+        System.out.println("========= Part 9 - A ===========");
+        String [] words = {"ABC", "AAAAA", "BBB", "kjhdsfkjo","JDSDFHKJH", "FGH","LXCVKJ","XLCKVJL","VCKJLK","VLK;LK","VLKL"};
+        ff.returnTheLongestString(words);
+        System.out.println("========= Part 9 - B ===========");
+        ff.findTheWord(words,"LXCV");
 
+        System.out.println("========= Part 10 ===========");
+        String[][] grades ={
+                {"David","85"},
+                {"Moshe","76"},
+                {"ILana","45"},
+                {"Shlomo","91"},
+                {"Hanna","82"}};
+        ff.findTheBestStudent(grades);
+        System.out.println("========= Part 11 ===========");
+        ff.findSecondBestNumber(numbers);
+        System.out.println("========= Part 12 ===========");
+        int [] numbers2 = {1,2,3,4,5,6,7,8,9,10};
+        int[] numbers3 = {10,2,3,6,5,4,7,8,9,1};
+        ff.theArrayAreEqual(numbers2,numbers3);
+        System.out.println("========= Part 13 ===========");
+        ff.returnPrimeNumbers(numbers2);
+        System.out.println("========= Part 14 ===========");
+        System.out.println(ff.convertToEnglish(123));
     }
 }
